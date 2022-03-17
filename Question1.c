7b8710197937bb4972a259c43fd45f0a57c3145d
alexvanrooy <96751896+alexvanrooy@users.noreply.github.com>
2022-03-17 18:09:41 -0400

    updated question1.c to initialize the "available" array

--- Question1.c
+++ Question1.c
@@ -2,7 +2,30 @@
 #include<stdlib.h>
 #include<unistd.h>
 
-int main(){
+
+int *available;
+int resource_num;
+
+int main(int argc, char *argv[]){
+
+
+	//initializes the available array
+	resource_num = argc-1;
+	available = (int*)malloc((argc - 1) * sizeof(int));
+
+	for(int i = 1 ; i < argc; i++){
+		available[i - 1] = atoi(argv[i]);
+	}
+
+	for(int i = 0; i < resource_num;i++ ){
+		printf("NUMBER: %d\n", available[i]);
+	}
+
+
+
+
+
+
 
 	return 0;
 }
